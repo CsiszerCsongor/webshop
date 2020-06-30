5df02b5f42e6693db5daa38d67a6f5792ab363da
Csiszer Csongor <csongor.zeek@gmail.com>
2020-06-30 17:33:22 +0200

    Add formatted search bar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BigNewsComponent } from './big-news/big-news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: BigNewsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { BigNewsComponent } from './big-news/big-news.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    ProductCardComponent,
+    FooterBarComponent,
+    BigNewsComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],

8b8de45e3a3f5aea00520771f6a841e2e7093a2c
Csiszer Csongor <csongor.zeek@gmail.com>
2020-07-15 12:28:03 +0200

    Design page

--- frontend/src/app/big-news/big-news.component.ts
+++ frontend/src/app/big-news/big-news.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigNewsComponent implements OnInit {
 
+  showSubmenuDiv: boolean = false;
+  cursorOnSubmenu: boolean = false;
+
+  delay2 = (ms: number) => new Promise(res => setTimeout(res, ms));
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async delay(ms: number) {
+    await new Promise(resolve => setTimeout(()=>resolve(), ms)).then(()=>console.log("fired"));
+  }
+
+  setSubmenuDisplayTrue(): void {
+    this.showSubmenuDiv = true;
+  }
+
+  setSubmenuFalseAfterDelay(): void{
+    this.delay(500).then(() => {
+      console.log("*");
+      if(!this.cursorOnSubmenu){
+        this.showSubmenuDiv = false;
+      }
+    });
+  }
+
+  cursorNotOnSubmenu(): void {
+    if(this.showSubmenuDiv)
+    this.showSubmenuDiv = false;
   }
 
 }
